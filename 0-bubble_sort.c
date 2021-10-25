f71abfa2d18a723c8d7725e18db32d2dfa01bb6a
jeremy302 <olajeremy123@gmail.com>
2021-10-25 13:34:08 +0100

    optimized

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,19 +11,21 @@
 void bubble_sort(int *array, size_t size)
 {
 	int tmp;
-	unsigned int i = 0, j = 0;
+	unsigned int i = 0, j = 1, _j = 1;
 	char sorted = 1;
 
 	if (array != NULL)
 		do {
-			for (sorted = 1, i = 0, ++j; i < size - j; ++i)
+			for (sorted = 1, i = 0; i < size - j; ++i)
 				if (array[i] > array[i + 1])
 				{
 					sorted = 0;
+					_j = size - i;
 					tmp = array[i];
 					array[i] = array[i + 1];
 					array[i + 1] = tmp;
 					print_array(array, size);
 				}
+			j = _j;
 		} while (!sorted);
 }
